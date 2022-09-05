6cc4643e725416049b9423ba788ceccc8250259c
ThomasDoughty <61171445+ThomasDoughty@users.noreply.github.com>
2022-09-05 17:36:04 -0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,18 @@
+# Mad lib
+#input a noun
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+#adjective = input("Enter an adjective:  ")
+celebrity = input("Enter a celebrity:   ")
+
+
+## madlib
+print("Peter Rabbit decided to go and visit his friend " + celebrity)
+print("He took a " + noun, " and " + verb, " it at " + celebrity, "'s head.")
+print("The " + noun, " hit him in the face.")
+print("'How do you like that " + noun, " he yelled")
+
+
+# on the way over he spotted a noun who needed a ride
+# he said hey there noun want to come verb with me and my friend noun?
+# the noun said yes
